304c6e9508bd710312970da216f973de50a4dc64
mrfurm4n <mr.furmand@gmail.com>
2022-10-30 20:12:25 +0200

    hello world in c++

--- src/main.cpp
+++ src/main.cpp
@@ -5,10 +5,9 @@
 //  Created by Данил Фурман on 30.10.2022.
 //
 
-#include <iostream>
-
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+#include<stdio.h>
+int main()
+{
+    printf("Hello, World!\n");
     return 0;
 }
