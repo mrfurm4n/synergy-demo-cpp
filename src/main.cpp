a4661fe1720445dd0db463fa6b3a23fcc0999a18
mrfurm4n <mr.furmand@gmail.com>
2022-11-01 09:37:26 +0200

    refactor after review

--- src/main.cpp
+++ src/main.cpp
@@ -4,10 +4,37 @@
 //
 //  Created by Данил Фурман on 30.10.2022.
 //
+#include <stdio.h>
 
-#include "./task-1/index.hpp"
-#include "./task-2/index.hpp"
+// Task 2
+void compareNumbers () {
+    int first, second;
+    
+    // Onboarding
+    printf("\nПрограмма сравнивает два числа. Вам будет предложено ввести два числа, после чего программа сравнит эти числа и выведет результат сравнения чисел на экран\n");
+    
+    // Input
+    printf("\nПожалуйста, введите первое число: ");
+    scanf("%i", &first);
+    printf("\nПожалуйста, введите второе число: ");
+    scanf("%i", &second);
+    
+    // Compare
+    if (first > second) {
+        printf("\nПервое число %i больше, чем второе число %i\n", first, second);
+    } else if (first < second) {
+        printf("\nПервое число %i меньше, чем второе число %i\n", first, second);
+    } else {
+        printf("\nПервое число %i и второе число %i равны\n", first, second);
+    }
+}
 
+// Task 1
+void printHello () {
+    printf("Hello, World!\n");
+}
+
+// Init all
 int main () {
     int programChoice;
 
