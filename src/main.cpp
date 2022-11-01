75484fbcb0b773c0b526551251bca16c63430b21
mrfurm4n <mr.furmand@gmail.com>
2022-11-01 12:09:03 +0200

    added user auth

--- src/main.cpp
+++ src/main.cpp
@@ -5,6 +5,55 @@
 //  Created by Данил Фурман on 30.10.2022.
 //
 #include <stdio.h>
+#include <string.h>
+//int strcmp (const char *str1, const char *str2);
+
+// Task 3
+void authUser () {
+    char userLoginInput[] = "";
+    int userFirstPassInput, userSecondPassInput;
+    
+    
+    // Users data
+    char firstUserLogin[] = "first";
+    int firstUserFirstPass = 123;
+    int firstUserSecondPass = 345;
+    
+    char secondUserLogin[] = "second";
+    int secondUserFirstPass = 345;
+    int secondUserSecondPass = 123;
+    
+    // Onboarding
+    printf("\nПрограмма авторизовывает пользователя. Вам будет предложено ввести ваш логин и два числа-пароля, после чего программа предоставит вам доступ или выдаст ошибку доступа\n");
+    
+    // Input
+    printf("\nПожалуйста, введите ваш логин в системе: ");
+    scanf("%str", &userLoginInput);
+    printf("\nПожалуйста, введите первый число-пароль: ");
+    scanf("%i", &userFirstPassInput);
+    printf("\nПожалуйста, введите второй число-пароль: ");
+    scanf("%i", &userSecondPassInput);
+    
+    // Auth
+    bool isFirstLoginEqual = strcmp(firstUserLogin, userLoginInput) == 0;
+    bool isSecondLoginEqual = strcmp(secondUserLogin, userLoginInput) == 0;
+    
+    if (isFirstLoginEqual) {
+        if ( (firstUserFirstPass == userFirstPassInput) && (firstUserSecondPass == userSecondPassInput) ) {
+            printf("\nПоздравляем, вы авторизованы в системе!\n");
+        } else {
+            printf("\nК сожалению, вам отказано в доступе. Попробуйте ещё раз...\n");
+        }
+    } else if (isSecondLoginEqual) {
+        if ( (secondUserFirstPass == userFirstPassInput) && (secondUserSecondPass == userSecondPassInput) ) {
+            printf("\nПоздравляем, вы авторизованы в системе!\n");
+        } else {
+            printf("\nК сожалению, вам отказано в доступе. Попробуйте ещё раз...\n");
+        }
+    } else {
+        printf("\nК сожалению, пользователя с такими логином не найдено. Попробуйте ещё раз...\n");
+    }
+}
 
 // Task 2
 void compareNumbers () {
@@ -47,6 +96,10 @@ int main () {
 
     if (programChoice == 2) {
         compareNumbers();
+    }
+    
+    if (programChoice == 3) {
+        authUser();
     }
 
     printf("\nКонец программы\n");
