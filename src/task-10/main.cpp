ddd1dd44119df0145b8020cbd3f195af3539b512
mrfurm4n <mr.furmand@gmail.com>
2022-12-03 12:44:23 +0200

    added task-10

---
+++ src/task-10/main.cpp
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+int main()
+{
+	// onboarding
+	printf("Вам будет предложенно ввести длину одномерного массива, а затем элементы массива. После чего программа выведет элементы массива на экран \n\n");
+
+	// input array length
+	int arrayLength;
+	printf("Введите длину массива: ");
+	scanf("%i", &arrayLength);
+
+	// input array elems
+	int arr[arrayLength];
+	printf("\n\nВведите элементы массива друг за другом:\n");
+
+	for (int i = 0; i < arrayLength; i += 1)
+	{
+		scanf("%1i", &arr[i]);
+	}
+
+	// print array elems
+	printf("\n\nВывод элементов массива:\n");
+	for (int i = 0; i < arrayLength; i += 1)
+	{
+		printf("%4i", arr[i]);
+	}
+
+	// end
+	printf("\n\nКонец программы.\n");
+	return 0;
+}
